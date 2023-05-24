e3993a1e769af8853db746e5533862e804365a65
ALQAWASMEH <omar.alqawasmeh@capgemini.com>
2023-05-24 12:09:05 +0200

    updated queries

--- queries.py
+++ queries.py
@@ -1,1 +1,22 @@
-print ("hello world")
+from SPARQLWrapper import SPARQLWrapper, JSON
+import pandas as pd
+
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
+# From https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries/examples#Cats
+sparql.setQuery("""
+SELECT ?item ?itemLabel
+WHERE
+{
+    ?item wdt:P31 wd:Q146 .
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+}
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+
+results_df = pd.json_normalize(results['results']['bindings'])
+results_df[['item.value', 'itemLabel.value']].head()
+
+print((results_df))
